08c29ebb198d067e1031f3cef4b0a946de8ba445
arthur_hds <arthurhenrique.thor@gmail.com>
2024-06-05 23:46:49 -0300

    refactor: Line is now a component that separes the divs

---
+++ src/app/components/item-line/item-line.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-line',
+  templateUrl: './item-line.component.html',
+  styleUrl: './item-line.component.scss'
+})
+export class ItemLineComponent {
+
+}
